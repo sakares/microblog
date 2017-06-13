78d2b5719750b9582dc90ed570f550771a758f47
sakares <jobbeins@gmail.com>
2017-06-13 16:44:20 +0700

    fix some syntax and edit few proper html

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :contents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to => "contents#index"
+  root to: 'contents#index'
 
 end
